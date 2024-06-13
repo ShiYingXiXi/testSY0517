229160ca2de55f03c724a94b0d1fbba6ecd8eb4a
ShiYing <shiyingxixi@163.com>
2024-06-13 17:45:03 +0800

    提交

--- build.sh
+++ build.sh
@@ -2,18 +2,22 @@
 
 set -xe
 
-Time=$(date +"%Y%m%d%H%M%S")
+#Time=$(date +"%Y%m%d%H%M%S")
 # test 文件名必须以 test_ 开头，否则会找不到
 TestDir="./testcases"
 ReportDir="./.build/allure-report"
 ResultDir="./.build/allure-results"
-CurrentResultDir="$ResultDir/$Time"
+#CurrentResultDir="$ResultDir/$Time"
 Environment="./environment.properties"
 
 mkdir -p "$CurrentResultDir"
 
-python main.py "$TestDir" "$CurrentResultDir"
-cp $Environment "$CurrentResultDir"
+#python main.py "$TestDir" "$CurrentResultDir"
+python main.py "$TestDir"
+#cp $Environment "$CurrentResultDir"
+cp $Environment "$ResultDir"
 
-allure generate --clean -o $ReportDir "$CurrentResultDir"
-allure serve "$CurrentResultDir"
+#allure generate --clean -o $ReportDir "$CurrentResultDir"
+allure generate --clean -o $ReportDir "$ResultDir"
+#allure serve "$CurrentResultDir"
+allure serve "$ResultDir"
