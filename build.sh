91b9eaa00e622a2cf0d4b8bde04b215dd7f29b5e
ShiYing <shiyingxixi@163.com>
2024-06-14 09:25:06 +0800

    提交1

--- build.sh
+++ build.sh
@@ -2,22 +2,22 @@
 
 set -xe
 
-#Time=$(date +"%Y%m%d%H%M%S")
+Time=$(date +"%Y%m%d%H%M%S")
 # test 文件名必须以 test_ 开头，否则会找不到
 TestDir="./testcases"
 ReportDir="./.build/allure-report"
 ResultDir="./.build/allure-results"
-#CurrentResultDir="$ResultDir/$Time"
+CurrentResultDir="$ResultDir/$Time"
 Environment="./environment.properties"
 
-#mkdir -p "$CurrentResultDir"
-#python main.py "$TestDir" "$CurrentResultDir"
-python main.py "$TestDir" "$ResultDir"
-#cp $Environment "$CurrentResultDir"
-cp $Environment "$ResultDir"
+mkdir -p "$CurrentResultDir"
+python main.py "$TestDir" "$CurrentResultDir"
+
+cp $Environment "$CurrentResultDir"
+#cp $Environment "$ResultDir"
 
 
-#allure generate --clean -o $ReportDir "$CurrentResultDir"
-allure generate --clean -o $ReportDir "$ResultDir"
-#allure serve "$CurrentResultDir"
-allure serve "$ResultDir"
+allure generate --clean -o $ReportDir "$CurrentResultDir"
+#allure generate --clean -o $ReportDir "$ResultDir"
+allure serve "$CurrentResultDir"
+#allure serve "$ResultDir"
